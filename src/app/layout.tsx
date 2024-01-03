c41bdb1c3539def118f9434d823add2895ad396d
Vivi Tseng <vivi.tseng@cardinalblue.com>
2024-01-03 14:58:42 -0800

    feat: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#CFA258" />
+      <meta name="msapplication-TileColor" content="#CFA258" />
+      <meta name="theme-color" content="#ffffff" />
       <GoogleAnalytics />
       <body>
         {children}
