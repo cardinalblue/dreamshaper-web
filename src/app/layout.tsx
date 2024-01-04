5bb1eadcda3d5771755b54b924c23fa80088c2d0
Vivi Tseng <vivi.tseng@cardinalblue.com>
2024-01-03 19:18:08 -0800

    feat: update metadata format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,16 @@ import { GoogleAnalytics } from '@/components/providers/GoogleAnalytics'
 const TITLE = 'AI Photo Style Transformer | Free & Easy-to-Use'
 const DESCRIPTION =
   'Instantly elevate your photos with our AI style transfer - a free online tool for magical image transformations. Experience the art of AI-driven photo styling in seconds.'
-const SITE_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+const SITE_URL = `https://${
+  (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production' && process.env.NEXT_PUBLIC_VERCEL_URL) ||
+  'www.photostyle.ai'
+}`
 const IMAGE_URL = `${SITE_URL}/og_image.png`
 
 export const metadata: Metadata = {
   title: TITLE,
   description: DESCRIPTION,
+  metadataBase: new URL(SITE_URL),
   openGraph: {
     title: TITLE,
     description: DESCRIPTION,
@@ -27,18 +31,40 @@ export const metadata: Metadata = {
     description: DESCRIPTION,
     images: [IMAGE_URL],
   },
+
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'manifest',
+      url: '/favicon/site.webmanifest',
+    },
+    {
+      rel: 'mask-icon',
+      url: '/favicon/safari-pinned-tab.svg',
+      color: '#CFA258',
+    },
+  ],
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#CFA258" />
-      <meta name="msapplication-TileColor" content="#CFA258" />
-      <meta name="theme-color" content="#ffffff" />
       <GoogleAnalytics />
       <body>
         {children}
