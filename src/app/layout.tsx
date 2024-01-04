c69ec9734b052a62925cead0f2ff7823c87444aa
Vivi Tseng <vivi.tseng@cardinalblue.com>
2024-01-03 19:55:50 -0800

    feat: update site url logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,10 @@ import { GoogleAnalytics } from '@/components/providers/GoogleAnalytics'
 const TITLE = 'AI Photo Style Transformer | Free & Easy-to-Use'
 const DESCRIPTION =
   'Instantly elevate your photos with our AI style transfer - a free online tool for magical image transformations. Experience the art of AI-driven photo styling in seconds.'
-const SITE_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+const SITE_URL = `https://${
+  (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production' && process.env.NEXT_PUBLIC_VERCEL_URL) ||
+  'www.photostyle.ai'
+}`
 const IMAGE_URL = `${SITE_URL}/og_image.png`
 
 export const metadata: Metadata = {
