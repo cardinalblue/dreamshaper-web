355c5f767f0d4ba338f63d4f87e3ded3eadbda0d
Vivi Tseng <vivi.tseng@cardinalblue.com>
2024-01-07 17:43:15 -0800

    feat: update effect order & display

--- src/utils/styleList.ts
+++ src/utils/styleList.ts
@@ -28,107 +28,6 @@ export const STYLE_LIST_HIGHLIGHT = [
 
 export const STYLE_LIST_REST = [
   {
-    id: 'glowing-lights',
-    name: 'Glowing Lights',
-    src: '/images/effects/glowing-lights.png',
-    config: {
-      prompt:
-        'Santa card, beautiful acrylic painting, festive Christmas market at night, colorful stalls, carousel lights, holiday shopping, market magic, little snow, fairy tale town, Christmas Town hall with lights, less snowing, holiday festival, watercolor splash',
-      negative_prompt:
-        'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
-      num_inference_steps: 12,
-      strength: 0.9,
-      guidance_scale: 10,
-      max_dim_of_input: MAX_INPUT_SIZE,
-      controlnet_conditioning_scale: 0.8,
-      face_mask_threshold: 0.98,
-    },
-  },
-  {
-    id: 'holiday-outfit',
-    name: 'Holiday Outfit',
-    src: '/images/effects/holiday-outfit.png',
-    config: {
-      prompt: 'wearing red Christmas Santa hat, Elevated and elegant, stylish green jumper photo',
-      negative_prompt:
-        'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
-      num_inference_steps: 8,
-      strength: 1,
-      guidance_scale: 9,
-      max_dim_of_input: MAX_INPUT_SIZE,
-      controlnet_conditioning_scale: 1,
-      face_mask_threshold: 0.98,
-    },
-  },
-  {
-    id: 'silent-night',
-    name: 'Silent Night',
-    src: '/images/effects/silent-night.png',
-    config: {
-      prompt:
-        'Watercolor, Christmas in starry night, moonlight scene, faint, thin Christmas decorations, starry skies, night sky',
-      negative_prompt:
-        'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
-      num_inference_steps: 8,
-      strength: 1,
-      guidance_scale: 10,
-      max_dim_of_input: MAX_INPUT_SIZE,
-      controlnet_conditioning_scale: 1,
-      face_mask_threshold: 0.98,
-    },
-  },
-
-  {
-    id: 'white-christmas',
-    name: 'White Christmas',
-    src: '/images/effects/white-christmas.png',
-    config: {
-      prompt: 'Winter snow, winter wonderland, snowflakes, white snow vibes, winter hues',
-      negative_prompt:
-        'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
-      num_inference_steps: 8,
-      strength: 1,
-      guidance_scale: 10,
-      max_dim_of_input: MAX_INPUT_SIZE,
-      controlnet_conditioning_scale: 1,
-      face_mask_threshold: 0.98,
-    },
-  },
-  {
-    id: 'christmas-vibes',
-    name: 'Xmas Vibes',
-    src: '/images/effects/christmas-vibes.png',
-    config: {
-      prompt:
-        'Acrylic painting style, Christmas vibes, Christmas scene, Christmas theme, Christmas lighting, Christmas decoration',
-      negative_prompt:
-        'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
-      num_inference_steps: 8,
-      strength: 0.9,
-      guidance_scale: 10,
-      max_dim_of_input: MAX_INPUT_SIZE,
-      controlnet_conditioning_scale: 1,
-      face_mask_threshold: 0.98,
-    },
-  },
-  {
-    id: 'celebration',
-    name: 'Celebration',
-    src: '/images/effects/celebration.png',
-    config: {
-      prompt:
-        'glitter, sparkles, confetti, kira kira, shinny spark, celebration, glimmer, glare effect',
-      negative_prompt:
-        'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure,framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb,close up, weird colors, watermark, blur haze, red face, red cheek, white face',
-      num_inference_steps: 8,
-      strength: 0.8,
-      guidance_scale: 10,
-      max_dim_of_input: MAX_INPUT_SIZE,
-      controlnet_conditioning_scale: 1,
-      face_mask_threshold: 0.98,
-    },
-  },
-  {
     id: 'posterize',
     name: 'Posterize',
     src: '/images/effects/posterize.png',
@@ -226,6 +125,106 @@ export const STYLE_LIST_REST = [
       face_mask_threshold: 0.98,
     },
   },
+  {
+    id: 'glowing-lights',
+    name: 'Glowing Lights',
+    src: '/images/effects/glowing-lights.png',
+    config: {
+      prompt:
+        'Santa card, beautiful acrylic painting, festive Christmas market at night, colorful stalls, carousel lights, holiday shopping, market magic, little snow, fairy tale town, Christmas Town hall with lights, less snowing, holiday festival, watercolor splash',
+      negative_prompt:
+        'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
+      num_inference_steps: 12,
+      strength: 0.9,
+      guidance_scale: 10,
+      max_dim_of_input: MAX_INPUT_SIZE,
+      controlnet_conditioning_scale: 0.8,
+      face_mask_threshold: 0.98,
+    },
+  },
+  // {
+  //   id: 'holiday-outfit',
+  //   name: 'Holiday Outfit',
+  //   src: '/images/effects/holiday-outfit.png',
+  //   config: {
+  //     prompt: 'wearing red Christmas Santa hat, Elevated and elegant, stylish green jumper photo',
+  //     negative_prompt:
+  //       'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
+  //     num_inference_steps: 8,
+  //     strength: 1,
+  //     guidance_scale: 9,
+  //     max_dim_of_input: MAX_INPUT_SIZE,
+  //     controlnet_conditioning_scale: 1,
+  //     face_mask_threshold: 0.98,
+  //   },
+  // },
+  {
+    id: 'silent-night',
+    name: 'Silent Night',
+    src: '/images/effects/silent-night.png',
+    config: {
+      prompt:
+        'Watercolor, Christmas in starry night, moonlight scene, faint, thin Christmas decorations, starry skies, night sky',
+      negative_prompt:
+        'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
+      num_inference_steps: 8,
+      strength: 1,
+      guidance_scale: 10,
+      max_dim_of_input: MAX_INPUT_SIZE,
+      controlnet_conditioning_scale: 1,
+      face_mask_threshold: 0.98,
+    },
+  },
+  // {
+  //   id: 'white-christmas',
+  //   name: 'White Christmas',
+  //   src: '/images/effects/white-christmas.png',
+  //   config: {
+  //     prompt: 'Winter snow, winter wonderland, snowflakes, white snow vibes, winter hues',
+  //     negative_prompt:
+  //       'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
+  //     num_inference_steps: 8,
+  //     strength: 1,
+  //     guidance_scale: 10,
+  //     max_dim_of_input: MAX_INPUT_SIZE,
+  //     controlnet_conditioning_scale: 1,
+  //     face_mask_threshold: 0.98,
+  //   },
+  // },
+  // {
+  //   id: 'christmas-vibes',
+  //   name: 'Xmas Vibes',
+  //   src: '/images/effects/christmas-vibes.png',
+  //   config: {
+  //     prompt:
+  //       'Acrylic painting style, Christmas vibes, Christmas scene, Christmas theme, Christmas lighting, Christmas decoration',
+  //     negative_prompt:
+  //       'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure, framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb, close up, weird colors, watermark, blur haze, red face, red cheek, white face',
+  //     num_inference_steps: 8,
+  //     strength: 0.9,
+  //     guidance_scale: 10,
+  //     max_dim_of_input: MAX_INPUT_SIZE,
+  //     controlnet_conditioning_scale: 1,
+  //     face_mask_threshold: 0.98,
+  //   },
+  {
+    id: 'celebration',
+    name: 'Celebration',
+    src: '/images/effects/celebration.png',
+    config: {
+      prompt:
+        'glitter, sparkles, confetti, kira kira, shinny spark, celebration, glimmer, glare effect',
+      negative_prompt:
+        'blurry, abstract, disfigured, deformed, cartoon, animated, toy, figure,framed, 3d, badly drawn hands, nude, cartoon, bad art, poorly drawn, extra limb,close up, weird colors, watermark, blur haze, red face, red cheek, white face',
+      num_inference_steps: 8,
+      strength: 0.8,
+      guidance_scale: 10,
+      max_dim_of_input: MAX_INPUT_SIZE,
+      controlnet_conditioning_scale: 1,
+      face_mask_threshold: 0.98,
+    },
+  },
+  // },
   // {
   //   id: 'cartoon-highlight',
   //   name: 'Cartoon Highlight',
